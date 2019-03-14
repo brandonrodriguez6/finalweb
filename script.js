087f7b9b3a21d17160cc123a4e07aff66b87ae7e
brandonrodriguez6 <37849270+brandonrodriguez6@users.noreply.github.com>
2019-03-14 12:40:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
-// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
-
-//
+//The var a/b/c applies the id "bab/bar/bac" to the div or button/image that is being clicked.
 var a = document.getElementById("bab");
 
-
+// The function showImage is the function that cycles images. The first if statement is cheecking to see if image baby is in div a which has the id "bab" Once the image baby is in place then it will switch to AJ. From there it will repeat the process and switch to the next image and then to the next to create a cycle of images.
 function showImage(){
 	if (a.src.match("images/baby.PNG")){
 		a.src = "images/aj.png";
@@ -41,15 +38,21 @@ function showImage3(){
 	}
 }
 function checkPosition(){
+//This is the main part of the function which acutally checks if the images are in place. The letter b,c, and a are the ids that the div has. To summarize the correct postions are rod2 in div b rod in div c and star is in div a.
 	if (b.src.match("images/rod2.png") && c.src.match("images/rod.png") && a.src.match("images/star.png")){
+		//the class opaque makes the images not visible when images are in the correct position. This function is adding a class
 			a.classList.add("opaque");
 			b.classList.add("opaque");
 			c.classList.add("opaque");
+		//Null makes the images not clickable anymore.
 			document.getElementById("bar").onclick = null;
 			document.getElementById("bac").onclick = null;
 			document.getElementById("bab").onclick = null;
+		//Plays audio when images are in the correct position.
 			document.getElementById("winner").play();
+		//This part of the function removes a class that makes the image with the id "bag" not appear will be removed.
 			document.getElementById("bag").classList.remove("none");
+		//This is the next part which adds the class "none" to the image that was previously in the div section. That makes the new image appear and takes over the place of the old image in the div.
 			document.getElementById("bad").classList.add("none");
 			
 	} else {}
